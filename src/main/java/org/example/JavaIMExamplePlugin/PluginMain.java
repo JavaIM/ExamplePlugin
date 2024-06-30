6a7e79eef23d1f4c809a75e1caae6ac7e3f468fb
AlexLiuDev233 <wzylin11@outlook.com>
2024-06-30 17:47:44 +0800

    feat: 适配最新JavaIM

--- src/main/java/org/example/JavaIMExamplePlugin/PluginMain.java
+++ src/main/java/org/example/JavaIMExamplePlugin/PluginMain.java
@@ -1,45 +1,50 @@
 package org.example.JavaIMExamplePlugin;
 
-import org.yuezhikong.newServer.ServerTools;
-import org.yuezhikong.newServer.plugin.Plugin.JavaPlugin;
-import org.yuezhikong.newServer.plugin.configuration.PluginConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yuezhikong.Server.ServerTools;
+import org.yuezhikong.Server.plugin.Plugin.JavaPlugin;
+import org.yuezhikong.Server.plugin.event.EventHandler;
+import org.yuezhikong.Server.plugin.event.Listener;
+import org.yuezhikong.Server.plugin.event.events.Server.ServerStartSuccessfulEvent;
 
-import java.util.Properties;
+// 在此处实现 Listener 接口仅是为在同一文件内演示 JavaIM 启动阶段
+// 您可以在其他类实现此接口，后在onLoad/onPreload(推荐在onLoad)注册
+public class PluginMain extends JavaPlugin implements Listener {
+    // 我们建议引入 lombok 依赖，并使用 @Slf4j 注解
+    private final static Logger log = LoggerFactory.getLogger(PluginMain.class);
 
-public class PluginMain extends JavaPlugin {
+    @Override
+    public void onPreload() {
+        // 此阶段被设计于所有于所有启动操作之前调用
+        // 此阶段，ServerAPI、ChatRequest、NetworkServer 等均未启动!
+    }
 
-    private Listener listener;
     @Override
     public void onLoad() {
-        ServerTools.getServerInstanceOrThrow().getLogger().info("[ExamplePlugin] 插件正在被加载");
-        PluginConfig.SaveDefaultConfiguration(this);//释放默认插件配置文件
-        Properties config = PluginConfig.getConfiguration(this);
-        if (config == null) {
-            ServerTools.getServerInstanceOrThrow().getLogger().error("无法获取配置文件");
-        }
-        else {
-            if (config.getProperty("Enable-Example-Command-System").equals("true")) {
-                if (!ServerTools.getServerInstanceOrThrow().getPluginManager().
-                        RegisterCommand("test", "一个测试命令",
-                                new TestCommand(), this)) {
-                    ServerTools.getServerInstanceOrThrow().getLogger().error("无法注册指令：/test");
-                }
-            }
-            if (config.getProperty("Enable-Example-Event-System").equals("true")) {
-                listener = new Listener();
-                ServerTools.getServerInstanceOrThrow().getPluginManager().AddEventListener(listener,this);
-            }
-        }
-        ServerTools.getServerInstanceOrThrow().getLogger().info("[ExamplePlugin] 插件加载成功");
+        // 此阶段被设计于启动过程中调用
+        // 与数据库启动、网络启动并行
+        // 请勿在此阶段操作网络、数据库等IO
+        log.info("[ExamplePlugin] 插件正在被加载");
+        ServerTools.getServerInstanceOrThrow().getPluginManager().addEventListener(this,this);
+        log.info("[ExamplePlugin] 插件加载成功");
+    }
+
+    @EventHandler
+    @SuppressWarnings("unused")
+    public void onServerStartSuccessful(ServerStartSuccessfulEvent event) {
+        // 此阶段被设计于启动结束后调用
+        // 此阶段，启动计时等已全部结束，用户已经可以进入
+        log.info("[ExamplePlugin] JavaIM 启动完成!");
     }
 
     @Override
     public void onUnload() {
-        ServerTools.getServerInstanceOrThrow().getLogger().info("[ExamplePlugin] 插件正在被卸载");
-        ServerTools.getServerInstanceOrThrow().getPluginManager().UnRegisterCommand("test");
-        if (listener != null)
-            ServerTools.getServerInstanceOrThrow().getPluginManager().RemoveEventListener(listener,this);
-        ServerTools.getServerInstanceOrThrow().getLogger().info("[ExamplePlugin] 插件卸载成功");
+        // 当插件卸载时
+        // 一般情况下，此阶段在用户踢出后IO断开前调用
+        log.info("[ExamplePlugin] 插件正在被卸载");
+        ServerTools.getServerInstanceOrThrow().getPluginManager().removeEventListener(this,this);
+        log.info("[ExamplePlugin] 插件卸载成功");
     }
 
 }
